34fe19d25e7bd9915865538542d62f5ef4a52b46
Boulay <cbmagala@gmail.com>
2024-10-07 10:01:37 +0200

    Router en construction des users

---
+++ routes/UserRouter.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import { getUser,updateUser,deleteUser,createUser,Connected } from '../controllers/UserController';
+
+const router = express.Router();
+
+// User routes
+
+router.get('/users',(req,res)=>{
+   
+    getUser
+} );
+
+
+router.put('/users/:id', updateUser);
+
+router.delete('/users/:id', deleteUser);
+
+router.post('/users',(req,res)=>{
+    let body = "";
+    req.on("data", (chunk) => {
+        body += chunk.toString();
+    });
+    req.on("end", () => {
+        const parsedBody =res.json(JSON.parse(body));
+        createUser(parsedBody.get(username), parsedBody.get(email), parsedBody.get(password)
+    ,(result) => {
+        res.WriteHead(201);
+        res.send("User created successfully");
+        res.end();
+    });
+
+    });
+});
+
+router.get('/connected', Connected);
+
+export default router;
