083abec87bc0847ca07e0a9574e2002ba66b28c9
Boulay <cbmagala@gmail.com>
2024-10-07 09:23:15 +0200

    init dossier

---
+++ server.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import fs from "fs";
+import mongoose from 'mongoose';
+const BdUrl="mongodb+srv://ClaudeB:Cyberbouffon5@cluster0.nc5na.mongodb.net/Blogify";
+const app = express();
+const Port =3000;
