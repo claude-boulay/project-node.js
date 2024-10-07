9e82a036e3bc4c074747853b89f03c6781ed00c2
COUBLANT <coublants@gmail.com>
2024-10-07 12:59:12 +0200

    app.use("/Blogify/posts/", PostRouter);

--- server.js
+++ server.js
@@ -2,21 +2,27 @@ import express from 'express';
 import fs from "fs";
 import mongoose from 'mongoose';
 import UserRouter from './routes/UserRouter.js';
+import PostRouter from './routes/PostRouter.js';
+
 const BdUrl="mongodb+srv://ClaudeB:Cyberbouffon5@cluster0.nc5na.mongodb.net/Blogify";
 const app = express();
 const Port =3000;
+
 mongoose.connect(BdUrl).then((result) => {
     console.log("MongoDB connected");
     app.listen(Port, () => console.log(`Server running on port ${Port}`));
-    
-  });
+});
 
-  app.use(express.json());
+app.use(express.json());
 
-  app.use("/Blogify/users/",UserRouter);
+// Route pour la page d'accueil
+app.get('/Blogify', (req, res) => {
+    res.sendFile('index.html', { root: 'views' }); 
+});
 
+app.use("/Blogify/users/", UserRouter);
+app.use("/Blogify/posts/", PostRouter);
 
-  app.use(function(req, res, next) {
+app.use(function(req, res, next) {
     res.status(404).send('Error 404 : Sorry cant find that!');
-  });
-
+});
