25a993ccfd8cf3fbed229079890780eb61ffae54
Boulay <cbmagala@gmail.com>
2024-10-07 09:25:04 +0200

    server start

--- server.js
+++ server.js
@@ -4,3 +4,14 @@ import mongoose from 'mongoose';
 const BdUrl="mongodb+srv://ClaudeB:Cyberbouffon5@cluster0.nc5na.mongodb.net/Blogify";
 const app = express();
 const Port =3000;
+mongoose.connect(BdUrl).then((result) => {
+    console.log("MongoDB connected");
+    app.listen(Port, () => console.log(`Server running on port ${Port}`));
+    
+  });
+
+
+  app.use(function(req, res, next) {
+    res.status(404).send('Error 404 : Sorry cant find that!');
+  });
+
