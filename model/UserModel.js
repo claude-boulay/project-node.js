c237e101e78355e6277410d2498feca31e55bf4b
Boulay <cbmagala@gmail.com>
2024-10-07 10:01:01 +0200

    Model pour les utilisateur

---
+++ model/UserModel.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const Userschema = new mongoose.Schema({
+        username: {type: String, required: true, unique: true},
+        password: {type: String, required: true},
+        email: {type: String, required: true, unique: true},
+      
+    });
+const UsersModel=new mongoose.model("Users", Userschema);
+export default UsersModel;
